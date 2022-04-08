15a7fbc61ef3b1a7e515de03dc4eb1a02464e729
6047198844 <jslee@miridih.com>
2022-04-08 17:31:05 +0900

    Initial commit

---
+++ src/main/java/com/example/inflearnspringmvc1corebackenditemservice/InflearnSpringMvc1CoreBackendItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringmvc1corebackenditemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringMvc1CoreBackendItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringMvc1CoreBackendItemServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inflearnspringmvc1corebackenditemservice/InflearnSpringMvc1CoreBackendItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringmvc1corebackenditemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringMvc1CoreBackendItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
